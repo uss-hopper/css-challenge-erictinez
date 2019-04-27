50e8361876a55872b029922c66fef54b770e265e
Eric Martinez <emartinez451@cnm.edu>
2019-04-27 14:05:43 -0600

    update

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 			<!-- ABOUT ME -->
 			<section class="about-me">
 				<div class="container">
-					<img src="images/stunning.jpg" alt="captain picard" />
+					<img src="images/2zlv80.jpg" alt="captain picard" />
 					<h2>A Day in the Life...</h2>
 					<p>Engage! Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
 					<p>Hiss and stare at nothing then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
